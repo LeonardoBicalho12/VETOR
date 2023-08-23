b3b820106e062d89237dcbf3a6cc37495c304d45
LeonardoBicalho12 <138938191+LeonardoBicalho12@users.noreply.github.com>
2023-08-22 22:27:34 -0300

    VETOR

---
+++ vetor/js/Untitled-1.js
@@ -0,0 +1,41 @@
+
+/* console.log("Não vou usar mais o celular enquanto meu professor estiver falando") */
+/* let aluno = ["Zé", "pezinho", "Sempre no CEL", "Um deixo o cel"]; */
+/* let nota1 = 1;
+let nota2 = 3;
+let nota4 = 8; */
+
+/* console.log(nota1); */
+
+
+/* console.log(aluno[1]); */
+/* método push */
+/* aluno.push("Alfredo");/* adiciona elemento no final da matriz (Arra)
+aluno.push("Charles");
+aluno.push("Paçoca");
+console.table(aluno);
+/* método unshift
+Adiciona um novo elemento no inicio da Array 
+aluno.unshift("Chaves"); 
+console.table(aluno);
+/* método pop()- que remove o último 
+aluno.pop("Zé")
+console.table(aluno)
+/* método shift() - remove o primeiro elemento 
+aluno.shift("Roberto")
+console.table(aluno) 
+/* adicionar o elemento em posição específica 
+aluno.splice(2, 0, "Guarulhos");
+console.table(aluno);
+ */
+let cadastro = [
+    ["Leonardo", 9, 10, 4, 8, 5],
+    ["Jova", 8, 7, 5, 10, 7],
+    ["Davi", 1, 2, 4, 6, 1],
+    ["Lys", 1, 3, 55, 88, 47],
+    ["Carnevale", 3, 44, 55, 11, 2]
+];
+console.table(cadastro);
+
+console.log(cadastro [2] [1]);
+
